97c6c16455208e463092c43512b70b3aa2cb5354
Arnaud <arnaudralec@gmail.com>
2024-09-18 18:43:04 +1100

    📝 chore: commenting main file

--- python_epub/python_epub.py
+++ python_epub/python_epub.py
@@ -1,9 +1,9 @@
 """Converts all epub files in ressources folder to pdf.
 
 Note : Some epub files are protected by DRMs. It is not illegal to remove the
-DRM protection, if it is for personnal usage only. I may look into that in the
-future, as there is some Python extensions to the Calibre Software that de-DRM
-protect epub files.
+DRM protection, if it is for personnal usage only. I may or may not look into
+that in the future, as there is some Python extensions to the Calibre Software
+that de-DRM protect epub files.
 """
 
 import os
